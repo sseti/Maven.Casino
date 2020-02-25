96aead684ebe5c0a5ed19b0dec4c81719df5a597
Zeth Kane <zkane@zipcodes-MacBook-Pro-9.local>
2020-02-25 16:00:09 -0500

    finished Chip

--- src/test/java/io/zipcoder/casino/games/specific/BlackJackTest.java
+++ src/test/java/io/zipcoder/casino/games/specific/BlackJackTest.java
@@ -1,4 +1,11 @@
 package io.zipcoder.casino.games.specific;
 
+import org.junit.Test;
+
 public class BlackJackTest {
+
+    @Test
+    public void newDealTest(){
+
+    }
 }
--- src/test/java/io/zipcoder/casino/models/ChipTest.java
+++ src/test/java/io/zipcoder/casino/models/ChipTest.java
@@ -1,4 +1,21 @@
 package io.zipcoder.casino.models;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChipTest {
+
+
+    @Test
+    public void dollarValTest(){
+
+        int expected = 1;
+        int actual = Chip.getWHITE();
+
+        Assert.assertEquals(expected, actual);
+
+    }
 }
