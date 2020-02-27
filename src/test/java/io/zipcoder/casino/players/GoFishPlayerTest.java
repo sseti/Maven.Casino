b4f8b1d95aa4e3ce62ffb9b97bf54c2a606082f3
Sandy Setiawan <ssetiawan@zipcodes-MacBook-Pro.local>
2020-02-27 16:03:05 -0500

    created test for gofishplayer

--- src/test/java/io/zipcoder/casino/players/GoFishPlayerTest.java
+++ src/test/java/io/zipcoder/casino/players/GoFishPlayerTest.java
@@ -1,4 +1,36 @@
 package io.zipcoder.casino.players;
 
+import io.zipcoder.casino.models.PlayingCard;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class GoFishPlayerTest {
+
+    @Test
+
+    public void goFishPlayerTest() {
+
+        boolean hasCard = true;
+
+        GoFishPlayer player = new GoFishPlayer("John");
+
+        PlayingCard card = new PlayingCard(PlayingCard.Rank.JACK, PlayingCard.Suit.HEARTS);
+
+        player.addCardToHand(card);
+
+        Assert.assertTrue(player.hasCard(card));
+    }
+
+    public void goFishPlayerTest2() {
+
+        boolean hasCard = true;
+
+        GoFishPlayer player = new GoFishPlayer("Bill");
+
+        PlayingCard card = new PlayingCard(PlayingCard.Rank.TEN, PlayingCard.Suit.DIAMONDS);
+
+        player.addCardToHand(card);
+
+        Assert.assertTrue(player.hasCard(card));
+    }
 }
