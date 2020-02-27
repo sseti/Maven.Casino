4b8e83ce1681d2f915e170a783fa069df51e768c
Sandy Setiawan <ssetiawan@zipcodes-MacBook-Pro.local>
2020-02-27 17:49:20 -0500

    started gofishnpc test

--- src/test/java/io/zipcoder/casino/players/GoFishNPCTest.java
+++ src/test/java/io/zipcoder/casino/players/GoFishNPCTest.java
@@ -1,4 +1,14 @@
 package io.zipcoder.casino.players;
 
+import org.junit.Test;
+
 public class GoFishNPCTest {
+
+    @Test
+    public void TestCat() {
+        GoFishNPC drawAmt = new GoFishNPC (5);
+        Integer actual = GoFishNPC.addCardTohand();
+        Integer expected = 6;
+        Assertions.assertEquals(expected, actual);
+    }
 }
